39092e7103dd932e40f5a6f511ca4bf7cafee4dd
rxw <1120309488@qq.com>
2018-05-23 22:47:50 +0800

    Add files via upload

---
+++ framework/Application.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rxw
+ * Date: 17-8-26
+ * Time: 下午8:45
+ */
+namespace framework;
+
+use framework\base\Container;
+
+class Application extends \framework\base\Application
+{
+    protected function addBaseComponents()
+    {
+        parent::addBaseComponents();
+
+        $components = [
+            'server' => 'framework\\server\\Server',
+            'msgTask' => 'blog\\conf\\Task',
+            'log' => 'framework\\components\\log\\Log',
+            'conf' => 'framework\\base\\Conf',
+            'cookie' => 'framework\\components\\cookie\\Cookie',
+            'response' => 'framework\\components\\response\\Response'
+        ];
+        if (PHP_SAPI == 'cli') {
+            $components['url'] = 'framework\\components\\url\\CliUrl';
+        }
+        $components = array_merge($components, $this->_conf['addComponentsMap'] ?? []);
+        $this->_container->addComponents(SYSTEM_APP_NAME, $components);
+
+        unset($components);
+    }
+
+    protected function beforeInit()
+    {
+        $this->_conf['components'] = $this->_conf['components']??[];
+        $this->_appConf['components'] = [];
+        $this->_conf['composer'] = $this->_conf['composer']??[];
+        $this->_appConf['composer'] = [];
+    }
+
+    public static function run($command = '')
+    {
+        $conf = [
+            'default' =>  require_file('framework/conf/base.php'),
+            'app' => []
+        ];
+        $instance = new static($conf);
+        $result = '';
+        try
+        {
+            $container = Container::getInstance();
+            $urlInfo = $container->getComponent(SYSTEM_APP_NAME, 'url')->run();
+            $_SERVER['CURRENT_SYSTEM'] = $urlInfo['system'];
+
+
+            if ($urlInfo !== false) {
+                // 初始化配置项
+                if (!$container->appHasComponents($urlInfo['system'])) {
+//                这里现在还缺少文件系统
+                    $appConf = require_file($urlInfo['system'] . '/conf/conf.php');
+                    $container->addComponents($urlInfo['system'], $appConf['addComponentsMap'] ?? []);
+                    $container->setAppComponents($urlInfo['system'] ,array(
+                        'components' => $appConf['components'] ?? [],
+                        'composer' => $appConf['composer'] ?? []
+                    ));
+                    unset($appConf);
+                }
+
+                $result = $container->getComponent(SYSTEM_APP_NAME, 'dispatcher')->run($urlInfo);
+                $container->getComponent(SYSTEM_APP_NAME, 'cookie')->send();
+                $container->getComponent(SYSTEM_APP_NAME, 'response')->send($result);
+                unset($result);
+            }
+        }
+        catch (\Throwable $e)
+        {
+            $code = $e->getCode() > 0 ? $e->getCode() : 500;
+            $response = $container->getComponent(SYSTEM_APP_NAME, 'response');
+            $response->setCode($code);
+            if (DEBUG) {
+                $result = $e->getMessage() . "\n trace: " . $e->getTraceAsString();
+            }
+            $response->send($result);
+            $instance->handleThrowable($e);
+            unset($default, $conf, $instance);
+
+        }
+    }
+}
--- framework/framework.php
+++ framework/framework.php
@@ -10,16 +10,5 @@ define('APP_ROOT', dirname(dirname(__FILE__)).'/');
 require __DIR__ . '/base/function.php';
 include __DIR__.'/autoloader.php';
 
-//$dis = new \framework\conformancehash\Dispatcher();
-////
-//$dis->addNode('192.168.1.100', '90');
-//$dis->addNode('192.118.2.100', '90');
-//$dis->addNode('192.128.3.100', '90');
-//$dis->addNode('192.128.2.100', '90');
-//$dis->addVirtualNode();
-//$dis->removeNode('192.168.1.100', '90');
-//var_dump($dis->_list->_nodeList);
-//$dis->findNextNodeByValue('dstrsffyhjhgfjghdjhfjdfdf');
-//$dis->findNextNodeByValue('dstsdfdf');
 
-\framework\web\Application::run($argv[1] ?? 'start');
+\framework\Application::run($argv[1] ?? 'start');
