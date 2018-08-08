6c84688a296c8ddbf3c11d152c4de8c58d33c73f
git <1120309488@qq.com>
2018-08-08 17:02:26 +0800

    change

--- framework/base/Documentor.php
+++ framework/base/Documentor.php
@@ -3,6 +3,7 @@ namespace framework\base;
 
 class Documentor extends Component
 {
+    const ALL = '__all__';
     protected $_docblock;
     protected $_docs;
     protected $_handle;
@@ -30,17 +31,35 @@ class Documentor extends Component
         return $this;
     }
 
-    public function getTags($name)
+    public function getTags($name = null)
     {
+        if (!$name) {
+          $name = static::ALL;
+        }
         if (isset($this->_docs[$this->_class][$this->_method][$name])) {
             return $this->_docs[$this->_class][$this->_method][$name];
         }
+        
         $tags = [];
-        foreach ($this->_docblock[$this->_class][$this->_method]->getTagsByName($name) as $item) {
+        if ($name == static::ALL) {
+          $_tags = $this->_docblock[$this->_class][$this->_method]->getTags();
+        } else {
+          $_tags = $this->_docblock[$this->_class][$this->_method]->getTagsByName($name);
+        }
+        
+        foreach ($_tags as $item) {
           if ($item instanceof \phpDocumentor\Reflection\DocBlock\Tags\Generic) {
-            $tags[] = $item->getDescription()->render();
+            if ($name == static::ALL) {
+              $tags[$item->getName()][] = $item->getDescription()->render();
+            } else {
+              $tags[] = $item->getDescription()->render();
+            }
           } else if ($item instanceof \phpDocumentor\Reflection\DocBlock\Tags\Method) {
-            $tags[] = $item->getMethodName();
+            if ($name == static::ALL) {
+              $tags[$item->getName()][] = $item->getMethodName();
+            } else {
+              $tags[] = $item->getMethodName();
+            }
           }
         }
         $this->_docs[$this->_class][$this->_method][$name] = $tags;
