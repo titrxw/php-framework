e8d7d767272ab301604282b61a2995eb0ad7e0a0
titrxw <1120309488@qq.com>
2018-10-23 22:35:26 +0800

    add file cache

---
+++ framework/components/cache/File.php
@@ -0,0 +1,279 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+namespace framework\components\cache;
+
+class File extends Cache implements CacheInterface
+{
+    protected $_expire;
+    protected $_cacheSubdir;
+    protected $_path;
+    protected $_dataCompress;
+    protected $_serialize;
+
+    
+    protected function init()
+    {
+        $this->_expire = $this->getValueFromConf('expire', 0);
+        $this->_cacheSubdir = $this->getValueFromConf('cache_subdir', false);
+        $this->_path = $this->getValueFromConf('path', '/runtime/cache');
+        $this->_dataCompress = $this->getValueFromConf('data_compress', false);
+        $this->_serialize = $this->getValueFromConf('serialize', true);
+
+        if (!is_dir($this->_path)) {
+            if (mkdir($this->_path, 0755, true)) {
+                return true;
+            }
+        }
+    }
+
+    /**
+     * 序列化数据
+     * @access protected
+     * @param  mixed $data
+     * @return string
+     */
+    protected function serialize($data)
+    {
+        if (is_scalar($data) || !$this->_serialize) {
+            return $data;
+        }
+
+        return serialize($data);
+    }
+
+    /**
+     * 反序列化数据
+     * @access protected
+     * @param  string $data
+     * @return mixed
+     */
+    protected function unserialize($data)
+    {
+        if ($this->_serialize) {
+            return unserialize($data);
+        } else {
+            return $data;
+        }
+    }
+
+    protected function getExpireTime($expire)
+    {
+        if ($expire instanceof \DateTime) {
+            $expire = $expire->getTimestamp() - time();
+        }
+
+        return $expire;
+    }
+
+    /**
+     * 取得变量的存储文件名
+     * @access protected
+     * @param  string $name 缓存变量名
+     * @param  bool   $auto 是否自动创建目录
+     * @return string
+     */
+    public function getCacheKey($name, $auto = false)
+    {
+        $name = hash('md5', $name);
+
+        if ($this->_cacheSubdir) {
+            // 使用子目录
+            $name = substr($name, 0, 2) . '/' . substr($name, 2);
+        }
+
+        $filename = APP_ROOT . ($this->_conf['prefix'] ?? '') . '/' . $this->_path . '/' . $name . '.php';
+        $dir      = dirname($filename);
+
+        if ($auto && !is_dir($dir)) {
+            mkdir($dir, 0755, true);
+        }
+        
+        return $filename;
+    }
+
+    /**
+     * 判断缓存是否存在
+     * @access public
+     * @param  string $name 缓存变量名
+     * @return bool
+     */
+    public function has($name)
+    {
+        return $this->get($name) ? true : false;
+    }
+
+    /**
+     * 读取缓存
+     * @access public
+     * @param  string $name 缓存变量名
+     * @param  mixed  $default 默认值
+     * @return mixed
+     */
+    public function get($name, $default = false)
+    {
+        $this->readTimes++;
+
+        $filename = $this->getCacheKey($name);
+
+        if (!is_file($filename)) {
+            return $default;
+        }
+
+        $content      = file_get_contents($filename);
+        $this->expire = null;
+
+        if (false !== $content) {
+            $expire = (int) substr($content, 8, 12);
+            if (0 != $expire && time() > filemtime($filename) + $expire) {
+                //缓存过期删除缓存文件
+                $this->unlink($filename);
+                return $default;
+            }
+
+            $this->expire = $expire;
+            $content      = substr($content, 32);
+
+            if ($this->_dataCompress && function_exists('gzcompress')) {
+                //启用数据压缩
+                $content = gzuncompress($content);
+            }
+            return $this->unserialize($content);
+        } else {
+            return $default;
+        }
+    }
+
+    /**
+     * 写入缓存
+     * @access public
+     * @param  string        $name 缓存变量名
+     * @param  mixed         $value  存储数据
+     * @param  int|\DateTime $expire  有效时间 0为永久
+     * @return boolean
+     */
+    public function set($name, $value, $expire = null)
+    {
+        if (is_null($expire)) {
+            $expire = $this->_expire;
+        }
+
+        $expire   = $this->getExpireTime($expire);
+        $filename = $this->getCacheKey($name, true);
+
+        $data = $this->serialize($value);
+
+        if ($this->_dataCompress && function_exists('gzcompress')) {
+            //数据压缩
+            $data = gzcompress($data, 3);
+        }
+
+        $data   = "<?php\n//" . sprintf('%012d', $expire) . "\n exit();?>\n" . $data;
+        $result = file_put_contents($filename, $data);
+
+        if ($result) {
+            clearstatcache();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 自增缓存（针对数值缓存）
+     * @access public
+     * @param  string    $name 缓存变量名
+     * @param  int       $step 步长
+     * @return false|int
+     */
+    public function inc($name, $step = 1)
+    {
+        if ($this->has($name)) {
+            $value  = $this->get($name) + $step;
+            $expire = $this->expire;
+        } else {
+            $value  = $step;
+            $expire = 0;
+        }
+
+        return $this->set($name, $value, $expire) ? $value : false;
+    }
+
+    /**
+     * 自减缓存（针对数值缓存）
+     * @access public
+     * @param  string    $name 缓存变量名
+     * @param  int       $step 步长
+     * @return false|int
+     */
+    public function dec($name, $step = 1)
+    {
+        if ($this->has($name)) {
+            $value  = $this->get($name) - $step;
+            $expire = $this->expire;
+        } else {
+            $value  = -$step;
+            $expire = 0;
+        }
+
+        return $this->set($name, $value, $expire) ? $value : false;
+    }
+
+    /**
+     * 删除缓存
+     * @access public
+     * @param  string $name 缓存变量名
+     * @return boolean
+     */
+    public function rm($name)
+    {
+        return $this->unlink($this->getCacheKey($name));
+    }
+
+    /**
+     * 清除缓存
+     * @access public
+     * @param  string $tag 标签名
+     * @return boolean
+     */
+    public function clear()
+    {
+        $files = (array) glob($this->_path . ($this->_conf['prefix'] ? $this->_conf['prefix'] . '/' : '') . '*');
+
+        foreach ($files as $path) {
+            if (is_dir($path)) {
+                $matches = glob($path . '/*.php');
+                if (is_array($matches)) {
+                    array_map('unlink', $matches);
+                }
+                rmdir($path);
+            } else {
+                unlink($path);
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 判断文件是否存在后，删除
+     * @access private
+     * @param  string $path
+     * @return bool
+     * @author byron sampson <xiaobo.sun@qq.com>
+     * @return boolean
+     */
+    private function unlink($path)
+    {
+        return is_file($path) && unlink($path);
+    }
+
+}
