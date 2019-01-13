29bb27d07094740fbdb952956ba7241b559262e4
git <1120309488@qq.com>
2019-01-13 16:54:33 +0800

    change

--- framework/base/Model.php
+++ framework/base/Model.php
@@ -16,6 +16,18 @@ class Model extends Component
         $this->afterInit();
     }
 
+    
+    protected function model($name)
+    {
+        $name = \ucfirst($name);
+        $componentModel = \md5(\getModule() .'/model/'.$name);
+        Container::getInstance()->addComponent(\getModule(), $componentModel,
+            \getModule() .'\\model\\'. $name, Container::getInstance()->getComponentConf(\getModule(), 'model'));
+//        在add之前设置当前model的conf
+//        待开发
+        return $this->getComponent(\getModule(), $componentModel);
+    }
+
     public function db()
     {
         if (!$this->_dbHandle) {
